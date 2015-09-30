ea6f29f581847f72d989ee49744930d8a7eeb4f3
lmandic <lmandic@hp-informatika.(none)>
2015-09-30 15:46:03 +0200

    Commit #9: Fixed isotope, made some code refactoring

---
+++ app/js/isotope-config.js
@@ -0,0 +1,22 @@
+$(function(){
+    var $galleryContent = $('.gallery__content');
+
+    $galleryContent.imagesLoaded(function(){
+        $galleryContent.removeClass('is-loading');
+        $galleryContent.isotope({
+            layoutMode: 'fitRows',
+            itemSelector: '.gallery__item',
+            percentPosition: true,
+            fitRows: {
+                gutter: 10
+            },
+            filter: '.salon'
+        });
+    });
+
+    $('.gallery__nav__item').on('click', function(){
+        var filter = $(this).attr('data-filter');
+        $galleryContent.isotope({ filter: filter });
+        $('a.active').removeClass('active');
+    });
+});
