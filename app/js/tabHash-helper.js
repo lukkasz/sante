d9b66bc6ffea50741bc97529ef234dd1e96fc967
ph0rest <ph0rest82@gmail.com>
2015-10-15 20:26:10 +0200

    Fixed some stuff

--- app/js/tabHash-helper.js
+++ app/js/tabHash-helper.js
@@ -8,25 +8,30 @@ $(function(){
         $(link).tab('show');
    });
 
-    //$(window).on('load', function(){
-    //     var loc = window.location;
-    //     var link;
-    //     if (loc.hash) {
-    //         nk = '#navTabs a[href="' + loc.hash + '"]';
-    //         ta$(link).tab('show');
-    //     }
-    // });
-   //};
+    $(window).on('load', function(e){
+        //e.preventDefault();
+        var loc = window.location;
+        var link;
+        if (loc.hash) {
+            link = '#navTabs a[href="' + loc.hash + '"]';
+            $(link).tab('show');
+        }
+    });
+
 });
 
 
 // this function takes hash from external link and open tab in service.html with that hash
-$(function(){
-    var loc = window.location;
-    var link;
+// $(function(){
+//     //e.preventDefault();
+//     var loc = window.location;
+//     var link;
 
-    if (loc.hash) {
-        link = '#navTabs a[href="' + loc.hash + '"]';
-        $(link).tab('show');
-    }
-});
+//     if (loc.hash) {
+//         link = '#navTabs a[href="' + loc.hash + '"]';
+//         $(link).tab('show');
+        
+//     }
+
+
+// });
